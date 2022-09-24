a6b4ad9e182501eebb188527ee5aed0c138ebb93
yymmyymm <109639114+yymmyymm@users.noreply.github.com>
2022-09-24 05:42:50 +0000

    original

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,6 +63,7 @@ Rails.application.configure do
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
+  config.active_job.queue_adapter = :inline
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
@@ -73,4 +74,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "482d799460274b63ac89d57ccade6227.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
