7a688c4ffcd721ce93a88837633d75545d259455
yymmyymm <109639114+yymmyymm@users.noreply.github.com>
2022-09-25 00:43:49 +0000

    9/25~ calendar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,32 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+//= require jquery
+//= require moment
+//= require fullcalendar
+
+$(function () {
+    // 画面遷移を検知
+    $(document).on('turbolinks:load', function () {
+        // lengthを呼び出すことで、#calendarが存在していた場合はtrueの処理がされ、無い場合はnillを返す
+        if ($('#calendar').length) {
+            function eventCalendar() {
+                return $('#calendar').fullCalendar({
+                });
+            };
+            function clearCalendar() {
+                $('#calendar').html('');
+            };
+
+            $(document).on('turbolinks:load', function () {
+                eventCalendar();
+            });
+            $(document).on('turbolinks:before-cache', clearCalendar);
+
+            $('#calendar').fullCalendar({
+                events: '/events.json'
+            });
+        }
+    });
+});
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_24_053123) do
+ActiveRecord::Schema.define(version: 2022_09_24_073129) do
+
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
